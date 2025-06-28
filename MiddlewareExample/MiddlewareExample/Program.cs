bf4c134712ef1340e8ab974afc52b26edd6ca30b
dev_local <devanshusingh55da@gmail.com>
2025-06-28 14:19:50 +0530

    Commit

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -45,20 +45,21 @@ app.UseAuthorization();
 
 app.Use(async (context, next) =>
 {
-	Console.WriteLine($"Logic before excuting the next delegate in the use method");
+	Console.WriteLine($"Logic before excuting the next delegate in the use method {1}");
 	await next.Invoke();
-	Console.WriteLine($"Logic after excuting the next delegate in the use method");
+	Console.WriteLine($"Logic after excuting the next delegate in the use method {2}");
 });
 
 app.Map("/usingmap", builder =>
 {
 	builder.Use(async (context, next) =>
 	{
-		Console.WriteLine("Map branch logic in the use method before the next delegate");
+		Console.WriteLine($"Map branch logic in the use method before the next delegate {3}");
 		await next.Invoke();
-		Console.WriteLine("map branch logic in the use method after the next delegate");
-		await context.Response.WriteAsync("Hello from the map branch");
+		Console.WriteLine($"map branch logic in the use method after the next delegate {4}");
+		await context.Response.WriteAsync($"Hello from the map branch {5}");
 	});
+
 
 	//builder.Run(async context =>
 	//{
@@ -67,10 +68,19 @@ app.Map("/usingmap", builder =>
 	//});
 });
 
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+{
+	builder.Use(async (context, next) =>
+	{
+		await next.Invoke();
+		await context.Response.WriteAsync($"Hello from the MapWhen branch {6}");
+	});
+});
+
 app.Run(async context =>
 {
-	Console.WriteLine("Writing the response to the client in the run method");
-	await context.Response.WriteAsync("Hello from the middleware component");
+	Console.WriteLine($"Writing the response to the client in the run method {7}");
+	await context.Response.WriteAsync($"Hello from the middleware component {8}");
 });
 
 app.MapControllers();
